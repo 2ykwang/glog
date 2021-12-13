fe1d7ca1d235017de59bba0d0f43c5003fcf962a
Yeonggwang Yang <csdp000@naver.com>
2021-12-13 22:24:39 +0900

    chore: 배포 환경 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,6 +19,14 @@ TEMPLATES_DIR = os.path.join(APPS_DIR, "templates")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(APPS_DIR, "media")
 
+
+STATIC_URL = "/static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, ".static_root")
+
 env = os.environ
 
 
