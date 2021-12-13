b0c5a5fcb168b451b4c3544b2b0681d7fa165a5e
Yeonggwang <immutable000@gmail.com>
2021-12-14 08:33:08 +0900

    chore: docker media volume 연결

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,4 @@ from .base import *  # noqa F403
 from .base import env
 
 # 지정된 호스트만 접속 가능 ( 반드시 명시해줘야함 )
-ALLOWED_HOSTS = env.get("ALLOWED_HOSTS", default="example.com").split(",")
-
-print(ALLOWED_HOSTS)
+ALLOWED_HOSTS = env.get("ALLOWED_HOSTS", default="*").split(",")
