e02f9dfc19eeb67f495b5601347e8fbb26c4b026
Yeonggwang <immutable000@gmail.com>
2021-12-13 19:04:10 +0900

    feat: post detail view 기반 코드 추가

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
+from blog.post import views
+
 app_name = "post"
 
-urlpatterns = [
-    
-]
+urlpatterns = [path("<str:post_slug>", views.post_detail, name="post_detail")]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_detail(request, post_slug):
+    return render(request, "post/detail.html", {"post_slug": post_slug})
